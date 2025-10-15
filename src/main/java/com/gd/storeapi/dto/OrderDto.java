3952c001e7402fd01f14cbd0a8e9746c58e94f78
IHOR KRAMARENKO <IHOR_KRAMARENKO@homedepot.com>
2025-10-15 14:12:23 +0200

    Add models

--- src/main/java/com/gd/storeapi/dto/OrderDto.java
+++ src/main/java/com/gd/storeapi/dto/OrderDto.java
@@ -1,5 +1,12 @@
 package com.gd.storeapi.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDto {
 
 }
--- src/main/java/com/gd/storeapi/model/Order.java
+++ src/main/java/com/gd/storeapi/model/Order.java
@@ -1,4 +1,4 @@
-package com.gd.storeapi.dto;
+package com.gd.storeapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
+public class Order {
 
 }
